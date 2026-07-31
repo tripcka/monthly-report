8de284ac7f48126944d9f170c2c95d4960e81930
tripicka <hello@tripicka.com>
2026-07-31 11:06:29 +0900

    update_260731_3

--- components/ChannelReportSection.jsx
+++ components/ChannelReportSection.jsx
@@ -5,6 +5,11 @@ import { buildNaverMediaBreakdownTable } from "../lib/channels";
 import { toImgArray } from "../lib/imageUtils";
 
 function OverviewSlide({ channel, data, hotelName }) {
+  const mediaBreakdownGroup = (channel.slideGroups || []).find((g) => g.type === "mediaBreakdown");
+  const hasMediaBreakdown = mediaBreakdownGroup && !!(data.tables?.pcSummary || data.tables?.moSummary);
+  const usedImageKeys = new Set((channel.slideGroups || []).flatMap((g) => g.imageKeys || []));
+  const overviewImages = channel.images.filter((img) => !usedImageKeys.has(img.key));
+
   return (
     <PageShell>
       <PageTitle kicker={channel.kicker} title={channel.title} />
@@ -15,23 +20,20 @@ function OverviewSlide({ channel, data, hotelName }) {
           ))}
         </StatCardRow>
       )}
+      {hasMediaBreakdown &&
+        (() => {
+          const { mediaTable } = buildNaverMediaBreakdownTable(data);
+          return <CsvTable label={mediaBreakdownGroup.title} rows={mediaTable} />;
+        })()}
+      {overviewImages.map((img) => (
+        <ImageSlot key={img.key} label={img.label} src={data.images[img.key]} />
+      ))}
       <Footer hotelName={hotelName} />
     </PageShell>
   );
 }
 
 function GroupSlide({ channel, group, data, hotelName }) {
-  if (group.type === "mediaBreakdown") {
-    const { mediaTable } = buildNaverMediaBreakdownTable(data);
-    return (
-      <PageShell>
-        <PageTitle kicker={channel.kicker} title={group.title} />
-        <CsvTable label="PC / 모바일 매체 비중" rows={mediaTable} />
-        <Footer hotelName={hotelName} />
-      </PageShell>
-    );
-  }
-
   const tablesInGroup = group.tableKeys.map((k) => channel.tables.find((t) => t.key === k)).filter(Boolean);
   const imagesInGroup = (group.imageKeys || [])
     .map((k) => channel.images.find((i) => i.key === k))
@@ -78,15 +80,21 @@ function SimpleSlide({ channel, data, hotelName }) {
 
 export default function ChannelReportSection({ channel, data, hotelName }) {
   if (channel.slideGroups) {
+    const mediaBreakdownGroup = channel.slideGroups.find((g) => g.type === "mediaBreakdown");
+    const hasMediaBreakdown = mediaBreakdownGroup && !!(data.tables?.pcSummary || data.tables?.moSummary);
+    const usedImageKeys = new Set(channel.slideGroups.flatMap((g) => g.imageKeys || []));
+    const hasOverviewImages = channel.images.some(
+      (img) => !usedImageKeys.has(img.key) && toImgArray(data.images[img.key]).length > 0
+    );
+    const showOverview = channel.kpis.length > 0 || hasMediaBreakdown || hasOverviewImages;
     return (
       <>
-        {channel.kpis.length > 0 && <OverviewSlide channel={channel} data={data} hotelName={hotelName} />}
+        {showOverview && <OverviewSlide channel={channel} data={data} hotelName={hotelName} />}
         {channel.slideGroups.map((group) => {
+          if (group.type === "mediaBreakdown") return null; // 개요 슬라이드에 이미 포함됨
           const hasData =
-            group.type === "mediaBreakdown"
-              ? !!(data.tables?.pcSummary || data.tables?.moSummary)
-              : group.tableKeys.some((k) => data.tables[k] && data.tables[k].length > 0) ||
-                (group.imageKeys || []).some((k) => toImgArray(data.images[k]).length > 0);
+            group.tableKeys.some((k) => data.tables[k] && data.tables[k].length > 0) ||
+            (group.imageKeys || []).some((k) => toImgArray(data.images[k]).length > 0);
           if (!hasData) return null;
           return <GroupSlide key={group.title} channel={channel} group={group} data={data} hotelName={hotelName} />;
         })}
